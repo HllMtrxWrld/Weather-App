34dc0c0915bd1b18793c020e5669fcc65d8d8021
Vitaliy Tymchuk <tymchuk94@gmail.com>
2023-02-06 11:56:33 +0000

    API request function was splited into 2 funcions : for current request and for 5 days forecast. Current day function was complited

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
+
     let cities = [];
     let todayContainer = $('#today');
     let forecastContainer = $('#forecast');
-    let now = new Date();
+    let currentDate = moment().format('DD/MM/YYYY');
 
-    setCityList();
-    ApiReques();
+   
     // function checkCitiesSaved() {
     //     // потрібна провірка чи не пусте сховище
     // }
@@ -17,7 +17,7 @@ $(document).ready(function () {
 function setLocalStorage() {
     
     if ($('#search-input').val().trim() !== "") {
-        cities.push($('#search-input').val().trim());
+        cities.unshift($('#search-input').val().trim());
         localStorage.setItem('cities', JSON.stringify(cities));
         $('#history-list').empty();
         setCityList();
@@ -28,7 +28,9 @@ function setLocalStorage() {
 function setCityList() {
     
     cities = JSON.parse(localStorage.getItem('cities'));
-    
+    // if (cities === []) {
+    //     return;
+    // }
     for (let i = 0; i < cities.length; i++) {
         let li = $('<li>');
         let bu = $('<button>').text(cities[i]);
@@ -39,38 +41,103 @@ function setCityList() {
 
 
 
-
-function ApiReques() {
-    for (let i = 0; i < cities.length; i++) {
-        
-        
+//  Function to get weather info about current request
+function getCurrentWeather() {
     
-
     let ApiKey = '56a78a793108c0e303b74692464ee285';
     let Limit = 1;
-//     let CityName = localStorage.getItem('Cities'); /*there is problem here*/
-    let GeocoderApiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cities[i] + "&limit=1&appid=" + ApiKey;
-$.ajax({
-    url: GeocoderApiURL,
-    method: 'GET'
-}).then(function (response) {
-    console.log(response);
-    let lat = response[0].lat;
-    let lon = response[0].lon;
-    console.log(lat, lon);
-
-
-   let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" +ApiKey;
+    let GeocoderApiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cities[0] + "&limit=1&appid=" + ApiKey;
     $.ajax({
-        url: queryURL,
-        method: "GET"
+        url: GeocoderApiURL,
+        method: 'GET'
     }).then(function (response) {
-        console.log(response);
-        console.log(response.city.name);
-        let todayName = $('<p>').text(response.city.name + now);
-        todayContainer.append(todayName);
+        let lat = response[0].lat;
+        let lon = response[0].lon;
+        let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + ApiKey + "&units=metric";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            let iconIndex = response.list[0].weather[0].icon;
+            let iconURL = 'http://openweathermap.org/img/w/' + iconIndex + ".png";
+            let weaterIcon = $('<img>').attr("src", iconURL);
+            let todayName = $('<h3>').text(response.city.name + ' ' + currentDate);
+            todayName.append(weaterIcon);
+            todayContainer.append(todayName);
+            let temp = $('<p>').text('Temp: ' + Math.round(response.list[0].main.temp) + '°C');
+            todayContainer.append(temp);
+            let wind = $('<p>').text('Wind: ' + response.list[0].wind.speed + ' KPH');
+            todayContainer.append(wind);
+            let humidity = $('<p>').text('Humidity: ' + response.list[0].main.humidity + '%');
+            todayContainer.append(humidity);
+        }); 
     });
-});
 };
+
+
+function getForecast() {
+
+    for (let i = 1; i < cities.length; i++) {
+            
+        let ApiKey = '56a78a793108c0e303b74692464ee285';
+        let Limit = 1;
+        let GeocoderApiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cities[i] + "&limit=1&appid=" + ApiKey;
+        $.ajax({
+            url: GeocoderApiURL,
+            method: 'GET'
+        }).then(function (response) {
+            let lat = response[0].lat;
+            let lon = response[0].lon;
+            let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + ApiKey + "&units=metric";
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+                // for (let i = 0; i < 5; i*8) {
+        
+//                  let iconIndex = response.list[0].weather[0].icon;
+//     let iconURL = 'http://openweathermap.org/img/w/' + iconIndex + ".png";
+    
+//     let weaterIcon = $('<img>').attr("src", iconURL);
+//     
+//     let todayName = $('<h3>').text(response.city.name + ' ' + currentDate + ' ' + weaterIcon);
+   
+//     let singleDayCard = $('<div>').addClass('card col-2 bg-primary');
+
+//     todayContainer.append(todayName);
+// }
+
+
+                // let iconIndex = response.list[0].weather[0].icon;
+                // let iconURL = 'http://openweathermap.org/img/w/' + iconIndex + ".png";
+                // let weaterIcon = $('<img>').attr("src", iconURL);
+                // let todayName = $('<h3>').text(response.city.name + ' ' + currentDate);
+                // todayName.append(weaterIcon);
+                // todayContainer.append(todayName);
+                // let temp = $('<p>').text('Temp: ' + Math.round(response.list[0].main.temp) + '°C');
+                // todayContainer.append(temp);
+                // let wind = $('<p>').text('Wind: ' + response.list[0].wind.speed + ' KPH');
+                // todayContainer.append(wind);
+                // let humidity = $('<p>').text('Humidity: ' + response.list[0].main.humidity + '%');
+                // todayContainer.append(humidity);
+            }); 
+
+        });
+    };
 };
+
+
+setCityList(); /* проблема з пустим значенням cities */
+getCurrentWeather();
+getForecast();
 });
+
+
+
+
+
+
+
