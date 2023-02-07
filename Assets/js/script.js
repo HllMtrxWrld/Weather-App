ea097654b18f405d2c18ee6e8cd73f383ef6422c
Vitaliy Tymchuk <tymchuk94@gmail.com>
2023-02-07 23:05:34 +0000

    local storage saving and start page functions were added. Also css file was added. the app is ready

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,143 +1,142 @@
-$(document).ready(function () {
-
-    let cities = [];
-    let todayContainer = $('#today');
-    let forecastContainer = $('#forecast');
-    let currentDate = moment().format('DD/MM/YYYY');
-
-   
-    // function checkCitiesSaved() {
-    //     // потрібна провірка чи не пусте сховище
-    // }
-    $('#search-button').on('click', function (event) {
-        event.preventDefault();
-        setLocalStorage();
-        });
-        
-function setLocalStorage() {
-    
-    if ($('#search-input').val().trim() !== "") {
-        cities.unshift($('#search-input').val().trim());
-        localStorage.setItem('cities', JSON.stringify(cities));
-        $('#history-list').empty();
-        setCityList();
-}
-};
-
-
-function setCityList() {
-    
-    cities = JSON.parse(localStorage.getItem('cities'));
-    // if (cities === []) {
-    //     return;
-    // }
-    for (let i = 0; i < cities.length; i++) {
-        let li = $('<li>');
-        let bu = $('<button>').text(cities[i]);
-        li.append(bu);
-        $('#history-list').append(li);
-    };
-};
+let ApiKey = '56a78a793108c0e303b74692464ee285';
+let city = '';
+let todayContainer = $('#today');
+let forecastContainer = $('#forecast');
+let currentDate = moment().format('DD/MM/YYYY');
 
 
 
-//  Function to get weather info about current request
-function getCurrentWeather() {
-    
-    let ApiKey = '56a78a793108c0e303b74692464ee285';
-    let Limit = 1;
-    let GeocoderApiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cities[0] + "&limit=1&appid=" + ApiKey;
+// Function to get current weather
+
+function getCurrentDay(city) {
+    let GeocoderApiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + ApiKey;
     $.ajax({
         url: GeocoderApiURL,
         method: 'GET'
+}).then(function (response) {
+    let lat = response[0].lat;
+    let lon = response[0].lon;
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + ApiKey + "&units=metric";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
     }).then(function (response) {
-        let lat = response[0].lat;
-        let lon = response[0].lon;
-        let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + ApiKey + "&units=metric";
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
+        let iconIndex = response.list[0].weather[0].icon;
+        let iconURL = 'http://openweathermap.org/img/w/' + iconIndex + ".png";
+        let weaterIcon = $('<img>').attr("src", iconURL);
+        let todayName = $('<h3>').text(response.city.name + ' ' + currentDate).addClass('todayEl');
+        todayName.append(weaterIcon);
+        todayContainer.append(todayName);
+        let temp = $('<p>').text('Temp: ' + Math.round(response.list[0].main.temp) + '°C').addClass('todayEl');
+        todayContainer.append(temp);
+        let wind = $('<p>').text('Wind: ' + response.list[0].wind.speed + ' KPH').addClass('todayEl');
+        todayContainer.append(wind);
+        let humidity = $('<p>').text('Humidity: ' + response.list[0].main.humidity + '%').addClass('todayEl');
+        todayContainer.append(humidity);
+        $('#today').addClass('today')
 
-            let iconIndex = response.list[0].weather[0].icon;
-            let iconURL = 'http://openweathermap.org/img/w/' + iconIndex + ".png";
-            let weaterIcon = $('<img>').attr("src", iconURL);
-            let todayName = $('<h3>').text(response.city.name + ' ' + currentDate);
-            todayName.append(weaterIcon);
-            todayContainer.append(todayName);
-            let temp = $('<p>').text('Temp: ' + Math.round(response.list[0].main.temp) + '°C');
-            todayContainer.append(temp);
-            let wind = $('<p>').text('Wind: ' + response.list[0].wind.speed + ' KPH');
-            todayContainer.append(wind);
-            let humidity = $('<p>').text('Humidity: ' + response.list[0].main.humidity + '%');
-            todayContainer.append(humidity);
-        }); 
-    });
+    }); 
+});
 };
 
 
-function getForecast() {
 
-    for (let i = 1; i < cities.length; i++) {
-            
-        let ApiKey = '56a78a793108c0e303b74692464ee285';
-        let Limit = 1;
-        let GeocoderApiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cities[i] + "&limit=1&appid=" + ApiKey;
-        $.ajax({
-            url: GeocoderApiURL,
-            method: 'GET'
-        }).then(function (response) {
-            let lat = response[0].lat;
-            let lon = response[0].lon;
-            let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + ApiKey + "&units=metric";
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function (response) {
-                console.log(response);
-                // for (let i = 0; i < 5; i*8) {
+
+
+//  Function to get 5 day weather forecast
+
+function getForecast(city) {
+    let GeocoderApiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + ApiKey;
+    $.ajax({
+        url: GeocoderApiURL,
+        method: 'GET'
+}).then(function (response) {
+    let lat = response[0].lat;
+    let lon = response[0].lon;
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + ApiKey + "&units=metric";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        for (let i = 0; i < 40; i+=8) {
+        let iconIndex = response.list[i].weather[0].icon;
+        let iconURL = 'http://openweathermap.org/img/w/' + iconIndex + ".png";
+        let weatherIcon = $('<img>').attr("src", iconURL);
+        let day = $('<h3>').text(response.list[i].dt_txt.split(' ', 1)).addClass('todayEl');
+        let cardContainer = $('<div>').addClass('cardContainer');
+        cardContainer.append(day);
+        cardContainer.append(weatherIcon);
+        forecastContainer.append(cardContainer);
+        let temp = $('<p>').text('Temp: ' + Math.round(response.list[0].main.temp) + '°C').addClass('todayEl');
+        cardContainer.append(temp);
+        let wind = $('<p>').text('Wind: ' + response.list[0].wind.speed + ' KPH').addClass('todayEl');
+        cardContainer.append(wind);
+        let humidity = $('<p>').text('Humidity: ' + response.list[0].main.humidity + '%').addClass('todayEl');
+        cardContainer.append(humidity);
+        }
+    }); 
+});
+};
+
+
+// Function to save searching history
+function setLocalStorage() {
+    let cities = JSON.parse(localStorage.getItem('cities'));
+    if (cities === null) {
+        let cities = [];
+        cities.push($('#search-input').val().trim());
+        localStorage.setItem('cities', JSON.stringify(cities));
+        let bu = $('<button>').text($('#search-input').val().trim()).addClass('history_button');
+        $('#history').append(bu);
+    } else if (cities.includes($('#search-input').val().trim())) {
         
-//                  let iconIndex = response.list[0].weather[0].icon;
-//     let iconURL = 'http://openweathermap.org/img/w/' + iconIndex + ".png";
-    
-//     let weaterIcon = $('<img>').attr("src", iconURL);
-//     
-//     let todayName = $('<h3>').text(response.city.name + ' ' + currentDate + ' ' + weaterIcon);
-   
-//     let singleDayCard = $('<div>').addClass('card col-2 bg-primary');
-
-//     todayContainer.append(todayName);
-// }
-
-
-                // let iconIndex = response.list[0].weather[0].icon;
-                // let iconURL = 'http://openweathermap.org/img/w/' + iconIndex + ".png";
-                // let weaterIcon = $('<img>').attr("src", iconURL);
-                // let todayName = $('<h3>').text(response.city.name + ' ' + currentDate);
-                // todayName.append(weaterIcon);
-                // todayContainer.append(todayName);
-                // let temp = $('<p>').text('Temp: ' + Math.round(response.list[0].main.temp) + '°C');
-                // todayContainer.append(temp);
-                // let wind = $('<p>').text('Wind: ' + response.list[0].wind.speed + ' KPH');
-                // todayContainer.append(wind);
-                // let humidity = $('<p>').text('Humidity: ' + response.list[0].main.humidity + '%');
-                // todayContainer.append(humidity);
-            }); 
-
-        });
+    } else {
+        cities.push($('#search-input').val().trim());
+        localStorage.setItem('cities', JSON.stringify(cities));
+        let bu = $('<button>').text($('#search-input').val().trim()).addClass('history_button');
+        $('#history').append(bu);
     };
 };
 
 
-setCityList(); /* проблема з пустим значенням cities */
-getCurrentWeather();
-getForecast();
+// Function to clear containers
+function cleanWeather() {
+    $('#today').empty();
+    $('#forecast').empty();
+}
+
+// Start function
+ function startPage() {
+    cleanWeather();
+    let cities = JSON.parse(localStorage.getItem('cities'));
+    if (cities !== null) {
+        for (let i = 0; i < cities.length; i++) {
+            
+            let bu = $('<button>').text(cities[i]).addClass('history_button');
+            $('#history').append(bu);
+            
+ }};
+ };
+
+
+//  Event listners for search button and history buttons
+ $(document).ready( function(){
+    startPage();
+$('#search-button').on('click', function (event) {
+    event.preventDefault();
+    cleanWeather();
+    getCurrentDay($('#search-input').val());
+    getForecast($('#search-input').val());
+    setLocalStorage();
+    $('#search-input').val('');
 });
-
-
-
-
-
-
-
+$('#history').on('click', 'button', function (event) {
+    event.preventDefault();
+    city = event.target.innerText.trim();
+    cleanWeather();
+    getCurrentDay(city);
+    getForecast(city);
+    $('#search-input').val('');
+})
+    
+ });
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Weather-App
+
+
+## Description
+This is a simple weather forecast application that allows a user to search a weather forecast for today and for next five days.
+The app does:
+
+Display the current day weather using API request;
+
+Display the weather for next five days using API request;
+
+Save the history of searching and let user easily come back to searched destination using local storage;
+
+Shows images representing weather;
+
+
+## Installation
+
+N/A
+
+## Usage
+
+In order to see the app follow the link. To look for weather type city name in search area and click search button. In orde to come back to serched city click on it in the history list.
+https://hllmtrxwrld.github.io/Weather-App/
+
+## Credits
+
+N/A
+![alt text](./Assets/img/screenShot.png.png)
+## License
+
+N/A
